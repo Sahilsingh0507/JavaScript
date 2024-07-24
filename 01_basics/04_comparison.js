48c7e456413aa7245e920b8bc60ab3dbc75fe7e5
Sahil Singh <83732513+Sahilsingh0507@users.noreply.github.com>
2024-07-24 12:13:29 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// comparison 
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// avoid these type of conversions 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+console.log("2" == 2); //true because it doesnot check the datatype 
+
+// strict check (===)
+console.log("2" === 2); //false because it check the datatype also 
