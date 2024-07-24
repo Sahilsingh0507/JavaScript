d5d6d3f8ec4646fb30a6137ba83c7b3a49b8e920
Sahil Singh <83732513+Sahilsingh0507@users.noreply.github.com>
2024-07-24 08:28:39 +0000

    datatypes

---
+++ 01_basics/02_datatTypes.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all JS code as newer version
+
+
+// we are using Node.js not the browser 
+// alert(3+3);
+// alert("Hello");
+
+
+// code readability should be high 
+// console.log("Sahil");
+// console.log(3+3);
+
+let name ="Sahil";
+let age  = 18;
+let isLoggedIn = false;
+let state;
+
+// primitive data types 
+//number =>2 to power 53;
+//bigint 
+// string => ""
+//boolean  => true/false
+//null => standalone value
+//undefined => value not assigned as of now 
+// symbol =>unique 
+
+
+//object 
+
+console.log(typeof "sahil");
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+
+console.log(typeof undefined); // undfined 
+console.log(typeof null); //object 
