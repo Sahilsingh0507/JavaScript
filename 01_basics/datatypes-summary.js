6e7c446f8f21e1ffb8cf412c0b60ac8d6ae456c4
Sahil Singh <83732513+Sahilsingh0507@users.noreply.github.com>
2024-07-24 12:41:48 +0000

    save changes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+// Data Type 
+// Primitive
+// call by value not by reference 
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// const score=100;
+// const scoreValue = 100.3;
+
+// const isLoggedIn = false ;
+// const outsideTemp = null;
+// let userEmail;
+// let userEmail = undefined;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+// bigint 
+// const bigNumber = 1664466164464464646156648949165156498489n;
+
+
+// in typescript we have to define datatype also 
+// const score:number = 100;
+
+
+// Reference type (or) Non Primitive data type 
+
+// Arrray, Objects , Functions 
+
+
