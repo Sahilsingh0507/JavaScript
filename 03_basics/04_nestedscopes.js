1cc8ba56e0c567321a66c73eaf19f479056158ef
Sahil Singh <83732513+Sahilsingh0507@users.noreply.github.com>
2024-07-28 17:56:01 +0000

    nested loop and function

--- 03_basics/04_nestedscopes.js
+++ 03_basics/04_nestedscopes.js
@@ -1,14 +1,46 @@
-function one() {
+// function one() {
+//     const username = "sahil";
+
+//     function two() {
+//         const website = "youtube";
+//         console.log(username);
+//     }
+//     // we cant access it here 
+//     // console.log(website);
+
+//     two();
+// }
+
+// one();
+
+if (true) {
     const username = "sahil";
-
-    function two() {
-        const website = "youtube";
-        console.log(username);
+    if (username === "sahil") {
+        const website = " youtube";
+        console.log(username + website);
     }
-    // we cant access it here 
     // console.log(website);
-
-    // two();
 }
 
-one();
+// console.log(username)
+
+
+// ********************Interesting *******************
+
+console.log(addOne(5));
+
+function addOne(num) {
+    return num + 1;
+}
+
+// addOne(5);
+
+
+// expression hai function nhi bss naam alag h kaam function wala hi h 
+// gives error because we stored the value returned by function in an variable 
+// addTwo(5);
+const addTwo = function (num) {
+    return num + 2;
+}
+
+addTwo(5);
