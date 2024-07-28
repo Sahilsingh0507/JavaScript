779936f3b73e8777f71e8e2d561d8b03cefae4f0
Sahil Singh <83732513+Sahilsingh0507@users.noreply.github.com>
2024-07-28 07:10:50 +0000

    nested scopes

---
+++ 03_basics/04_nestedscopes.js
@@ -0,0 +1,14 @@
+function one() {
+    const username = "sahil";
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+    // we cant access it here 
+    // console.log(website);
+
+    // two();
+}
+
+one();
