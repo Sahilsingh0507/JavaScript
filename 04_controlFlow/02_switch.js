a61bf4e6203d2e7a18202bd0a75096b528ff6bf5
Sahil Singh <83732513+Sahilsingh0507@users.noreply.github.com>
2024-07-30 16:05:57 +0000

    switch and truthy values

---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,57 @@
+// checking value by multiple condition 
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// if we dont use break in switch case it executes all the values present below it except default case 
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+const Month = "apr";
+switch (Month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+
+    default:
+        console.log("Default case")
+        break;
+}
---
+++ 04_controlFlow/03_truthyAndFalsy.js
@@ -0,0 +1,54 @@
+const userEmail = "Sahil@gmail.com";
+// if we dont give any value in the user email then we get output as dont have an user email 
+// we cant pass empty string 
+const userEmail1 = "";
+
+
+const userEmail2 = []
+
+if (userEmail2) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy value 
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy value 
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail2.length === 0) {
+//     console.log("Array is Empty");
+// }
+
+
+const emptyObj = {};
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish coalescing Operator (??): null undefined
+
+let val1;
+// check krta hai pehli jo value milti hai wahi return kr deta hai 
+val1 = 5 ?? 10;
+console.log(val1);
+
+//pehli value null aaya toh usne dusri value check krke return kr diya 
+val1 = null ?? 10;
+console.log(val1);
+
+// isme bhi pehle undefined pe gya toh usne check kiya next me jo value tha wo return kr diya 
+val1 = undefined ?? 20;
+console.log(val1);
+
+// isme check kiya usne null lekin next value usne check kiya toh usko mil gya isiliye second value return hua 
+val1 = null ?? 10 ?? 20;
+console.log(val1);
+
+// often we get confused between these two 
+// ternary operator 
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("greater than 80");
