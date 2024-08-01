5d4558fc952f15a40368a1d8851be092e9c7dd3c
Sahil Singh <83732513+Sahilsingh0507@users.noreply.github.com>
2024-08-01 14:58:19 +0000

    while loop

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,25 @@
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// let indexOfArray = 0;
+// while (indexOfArray < myArray.length) {
+//     console.log(`Value is ${myArray[indexOfArray]}`);
+//     indexOfArray = indexOfArray + 1;
+// }
+
+// do while loop 
+// let score = 1;
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
+let marks = 11;
+do {
+    console.log(`Marks is ${marks}`);
+    marks++;
+} while (marks <= 10);
